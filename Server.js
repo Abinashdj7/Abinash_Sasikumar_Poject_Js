5cb18ba3434b9ac603eea023fa84fa4341251d00
Abinashdj7 <150699472+Abinashdj7@users.noreply.github.com>
2025-03-21 11:47:34 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require("cors"); // For CORS
-const userRoutes = require("./routes/Users"); // Import user routes
+const cors = require("cors");
+const userRoutes = require("./routes/Users"); 
 const invoiceRoutes=require("./routes/Invoices")
 const productRoutes=require("./routes/Products")
 
@@ -10,23 +10,22 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
-// Middleware
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Parse incoming JSON requests
 
-// Database connection
+app.use(cors()); 
+app.use(express.json()); 
+
+n
 mongoose
     .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log("Error connecting to MongoDB:", err));
 
-// Use the user routes
+
 app.use("/api/users", userRoutes); 
 app.use('/api/invoices', invoiceRoutes);
 app.use("/api/products",productRoutes)
 
 
-// Default route
 app.get("/", (req, res) => {
     res.send("Welcome to the E-commerce API!");
 });
