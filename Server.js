3e6934938baf689890c9c0766ea1ef2fa19e428c
Abinashdj7 <150699472+Abinashdj7@users.noreply.github.com>
2025-03-21 11:51:27 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
-n
 mongoose
     .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
