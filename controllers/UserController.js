76c779730985a9a50bb1b9158266d31853236312
Abinashdj7 <150699472+Abinashdj7@users.noreply.github.com>
2025-03-21 11:44:50 +0100

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,23 +25,20 @@ const createUser = async (req, res) => {
 
 const updateUser = async (req, res) => {
   try {
-    // Check if the password is being updated
     if (req.body.password) {
-      // Hash the new password before saving it to the database
-      const salt = await bcrypt.genSalt(10); // Generate salt with cost factor 10
-      req.body.password = await bcrypt.hash(req.body.password, salt); // Hash the password
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt); 
     }
 
-    // Update the user with the new data (including hashed password if updated)
     const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json(user); // Respond with the updated user
+    res.status(200).json(user); 
   } catch (error) {
-    res.status(500).json({ message: error.message }); // Handle any errors
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -59,23 +56,19 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Find user by email
+   
     const user = await User.findOne({ email });
 
-    // If the user doesn't exist
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Compare the plain text password with the hashed password stored in the database
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-    // If the password is incorrect
     if (!isPasswordCorrect) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // If login is successful, return the user data
     res.json({ message: "Login successful", user });
   } catch (error) {
     res.status(500).json({ message: error.message });
