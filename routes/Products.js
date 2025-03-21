a11e06d5f68d6d97b73c3a7b858c1c6e4b54a02b
Abinashdj7 <150699472+Abinashdj7@users.noreply.github.com>
2025-03-21 11:46:38 +0100

    Update Products.js

--- routes/Products.js
+++ routes/Products.js
@@ -9,11 +9,11 @@ const {
   deleteProduct
 } = require('../controllers/ProductController');
 
-// Routes for handling products
-router.post('/', createProduct);             // Create a new product
-router.get('/', getProducts);               // Get all products
-router.get('/:id', getProductById);         // Get a product by ID
-router.put('/:id', updateProduct);         // Update a product by ID
-router.delete('/:id', deleteProduct);      // Delete a product by ID
+
+router.post('/', createProduct);          
+router.get('/', getProducts);             
+router.get('/:id', getProductById);    
+router.put('/:id', updateProduct);       
+router.delete('/:id', deleteProduct);     
 
 module.exports = router;
